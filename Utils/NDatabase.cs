20f68b452fee57d50b3e3acb15218c8a2d650230
fadhil_riyanto <id.fadhil.riyanto@gmail.com>
2021-12-26 09:30:14 +0700

    Nosql database class added

---
+++ Utils/NDatabase.cs
@@ -0,0 +1,9 @@
+
+
+// namespace Telegram.Bot.Fadhil_riyanto_bot.Utils{
+//     class NDatabase{
+//         using(var db = new LiteDatabase(@"MyData.db")){
+//             return;
+//         }
+//     }
+// }
