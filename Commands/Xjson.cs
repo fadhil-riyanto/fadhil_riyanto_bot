c88f2fd22ef223aaa4f98a8d40ef5f0db4305789
fadhil_riyanto <id.fadhil.riyanto@gmail.com>
2021-12-26 09:51:03 +0700

    json class

---
+++ Commands/Xjson.cs
@@ -0,0 +1,24 @@
+using Telegram.Bot.Types;
+using Newtonsoft.Json;
+namespace Telegram.Bot.Fadhil_riyanto_bot.Commands
+{
+    class Xjson
+    {
+        public static async Task<Message> Entry_point(ITelegramBotClient botClient, Message message)
+        {
+            var root = new xjsontostr { Messages = new List<Message> { message } };
+            string json = JsonConvert.SerializeObject(root, Formatting.Indented);
+
+            return await botClient.SendTextMessageAsync(chatId: message.Chat.Id, text: json, replyToMessageId: message.MessageId, parseMode: Types.Enums.ParseMode.Html);
+        }
+    }
+    public class xjsontostr
+    {
+
+        [JsonProperty("Message")]
+        public List<Message> Messages { get; set; }
+    }
+
+
+}
+
